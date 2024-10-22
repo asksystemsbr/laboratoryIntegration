982c9b3782e3d37499b404827e0cdf7c27f1bcca
TeiaoDias <teiao.dias@gmail.com>
2024-10-22 10:16:07 -0300

    Fonte

---
+++ ApiIntegracaoEntity/Log/Log.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiIntegracaoEntity.Log
+{
+    public class Log
+    {
+        public string id { get; set; }
+        public object documento {  get; set; }
+
+        public string partitionKey { get; set; }
+
+
+        public Log(object documento) {
+            id = Guid.NewGuid().ToString();
+            this.documento = documento;
+        }
+
+
+    }
+}
